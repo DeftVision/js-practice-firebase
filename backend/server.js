72db3f568860d02ac567037425051480c847991b
Gabriel <gjbarret@asu.edu>
2024-06-13 20:47:16 -0600

    create backend files
    + firebase model
    + firebase controller
    + firebase route
    - .env from gitignore
    + db connection

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 8001;
+const connectDB = require('./config/db');
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
